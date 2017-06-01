fdecb1def7e187a9e920a87f05a36c6243df156d
feng <liufeng_bupt@163.com>
2017-06-01 23:43:24 +0800

    使用注释编写了首个功能测试，而且使用了unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,29 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+	"""docstring for NewVisitorTest"""
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test')
+	# def __init__(self, arg):
+	# 	super(NewVisitorTest, self).__init__()
+	# 	self.arg = arg
+		
+
+if __name__ == '__main__':
+	unittest.main(warnings = 'ignore')
+
+
+# browser = webdriver.Firefox()
+# browser.get('http://localhost:8000')
+# assert 'To-Do' in browser.title, "Browser title was " + browser.title
+# browser.quit()
